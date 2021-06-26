61fcfe0a1cf54092adfbc2ee074081d0ad864172
Leong <leongbs34@gmail.com>
2021-06-26 23:42:39 +0800

    Completed

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-//press a key
-document.addEventListener('keydown', playSound);
-
-//play sound according to key
 function playSound(e){
-    switch(e.code){
-        case 'KeyA':
-            document.querySelector('audio[data-key="65"]').play();
-            break;
-        case 'KeyS':
-            document.querySelector('audio[data-key="83"]').play();
-            break;
-        case 'KeyD':
-            document.querySelector('audio[data-key="68"]').play();
-            break;
-        case 'KeyF':
-            document.querySelector('audio[data-key="70"]').play();
-            break;
-        case 'KeyG':
-            document.querySelector('audio[data-key="71"]').play();
-            break;
-        case 'KeyH':
-            document.querySelector('audio[data-key="72"]').play();
-            break;
-        case 'KeyJ':
-            document.querySelector('audio[data-key="74"]').play();
-            break;
-        case 'KeyK':
-            document.querySelector('audio[data-key="75"]').play();
-            break;
-        case 'KeyL':
-            document.querySelector('audio[data-key="76"]').play();
-            break;
-    }
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
 }
+
+function removeTransition(e){
+    if(e.propertyName != 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
